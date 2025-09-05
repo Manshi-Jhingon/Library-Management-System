bd045016196a3053a0574006f9ac9de267fe193d
Manshi Jhingin <manshijhingon@gmail.com>
2025-09-05 17:04:24 +0530

    Initialized

---
+++ frontend/src/manshi.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const manshi = () => {
+  return (
+    <div>
+        <p className='text-red-500'>Hye Developers...</p>
+      
+    </div>
+  )
+}
+
+export default manshi
